a0168d1363717f76721320b7d622be92b436362a
HansDP <hans_de_potter@hotmail.com>
2016-06-27 01:32:34 +0200

    Fixed tab to space

--- examples/globalsaga-pair-of-random-gif-viewers/src/saga-random-gif-viewer/sagas.js
+++ examples/globalsaga-pair-of-random-gif-viewers/src/saga-random-gif-viewer/sagas.js
@@ -5,36 +5,36 @@ import * as Effects from './effects';
 
 export default (local) => {
 
-	const getTopic = (globalState) => {
+  const getTopic = (globalState) => {
 		
-		// You can access the global state with the globalState argument
-		// return globalState.something
+    // You can access the global state with the globalState argument
+    // return globalState.something
 
-		// Local model can be found in local.getState()
-		return local.getState().topic
+    // Local model can be found in local.getState()
+    return local.getState().topic
    
-	}
+  }
 
-	function* fetchGif() {
+  function* fetchGif() {
 
-		const topic = yield select(getTopic)
-		const url = yield call(Effects.fetchGif, topic)
+    const topic = yield select(getTopic)
+    const url = yield call(Effects.fetchGif, topic)
 
-		// Putting global like you would in standard redux-saga
-		// yield put({ type: 'SOME_GLOBAL_ACTION' })
+    // Putting global like you would in standard redux-saga
+    // yield put({ type: 'SOME_GLOBAL_ACTION' })
 
-		// Putting local entry must be wrapped with local.type(...)
-		yield put({ type: local.type('NewGif'), payload: { url } })
+    // Putting local entry must be wrapped with local.type(...)
+    yield put({ type: local.type('NewGif'), payload: { url } })
 
-	}
+  }
 
-	return function* requestGif() {
+  return function* requestGif() {
 
-		// Taking something global like you would in standard redux-saga
-		// yield* takeEvery('GLOBAL_INTERESTING_ACTION', doSomething)
+    // Taking something global like you would in standard redux-saga
+    // yield* takeEvery('GLOBAL_INTERESTING_ACTION', doSomething)
 
-		// Taking something local must be wrapped with local.type(...)
-		yield* takeEvery(local.type('RequestGif'), fetchGif)
+    // Taking something local must be wrapped with local.type(...)
+    yield* takeEvery(local.type('RequestGif'), fetchGif)
 
-	}
+  }
 }
